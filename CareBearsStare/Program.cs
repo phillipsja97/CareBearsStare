95ad101d7f81527c05481b96f31a70141dcbc6ec
Jamie <phillipsja97@gmail.com>
2020-03-17 18:15:32 -0500

    Add project files.

---
+++ CareBearsStare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CareBearsStare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
